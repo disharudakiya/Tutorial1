4aeec3fb4f10087260855b512cc13133a4a061c9
disharudakiya <141400666+disharudakiya@users.noreply.github.com>
2025-07-01 21:22:41 +0530

    Add project files.

---
+++ Q2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial1
+{
+    internal class Q2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Name : ");
+            string name = Console.ReadLine();
+
+            Console.Write(" Enter your birthdate (dd/mm/yyyy) : ");
+            string date = Console.ReadLine();
+
+            Console.Write(" Enter your address : ");
+            string address = Console.ReadLine();
+            
+
+            Console.Write("Enter your City : ");
+            string city = Console.ReadLine();
+
+            Console.Write("Enter your Pincode :  ");
+            string PinInput = Console.ReadLine();
+            int pin = int.Parse(PinInput);
+
+            Console.Write("Enter state : ");
+            string state = Console.ReadLine();
+
+            Console.Write("Enter country : ");
+            string country = Console.ReadLine();
+
+            Console.Write("Enter Email address : ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("\n ============== PROFILE ==============");
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("DOB : " + date);
+            Console.WriteLine("Address :" + address);
+            Console.WriteLine("City : " + city);
+            Console.WriteLine("Pincode : " + pin);
+            Console.WriteLine("State : " + state);
+            Console.WriteLine("Country : " + country);
+            Console.WriteLine("Email : " + email);
+
+;
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Q3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial1
+{
+    internal class Q3
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number : ");
+            //string num = Console.ReadLine();
+            //int number = int.Parse(num);
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if(number % 2 == 0)
+            {
+                Console.WriteLine(number + " is a even number ");
+            }
+            else 
+            {
+                Console.WriteLine(number + " is odd number ");
+            }
+        }
+    }
+}
