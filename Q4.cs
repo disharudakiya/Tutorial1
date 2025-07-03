b30f3d30c9513d808fcb236560591ddeed45d898
disharudakiya <141400666+disharudakiya@users.noreply.github.com>
2025-07-03 14:17:18 +0530

    Q4

---
+++ Q4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial1
+{
+    internal class Q4
+    
+        
+
+
+        {
+            static void Main(string[] args)
+            {
+                int x;
+                Console.Write("Enter Number: ");
+                string str = Console.ReadLine();       // First read input
+                x = Convert.ToInt32(str);              // Then convert
+
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine("Number is Even");
+                }
+                else
+                {
+                    Console.WriteLine("Number is Odd");
+                }
+
+                Console.ReadLine();  
+            }
+        }
+    }
+
+
+
---
+++ Q5.cs
@@ -0,0 +1,42 @@
+﻿    using System; // use basic system functionalities
+
+    namespace ConsoleApplication1 // define a namespace for organization
+
+    {
+
+        class Program // define a class named Program
+
+        {
+
+            static void Main(string[] args) // main entry point of the program
+
+            {
+
+                int n, fact = 1; // declare variables for number and factorial
+
+                Console.WriteLine("Enter Number : "); // ask user to input a number 
+
+                string str = Console.ReadLine(); // read user input as a string
+                                            
+
+                n = Convert.ToInt32(str);  // Convert the string input to an integer
+
+                for (int i = 1; i <= n; i++) // loop from 1 to the input number
+
+                {
+
+                    fact = fact * i; // calculate factorial by multiplying each number
+
+                }
+
+                Console.WriteLine("Factorial : {0}", fact); // output the calculated factorial
+
+
+
+                Console.Read(); // wait for user input before closing the console window
+
+            }
+
+        }
+
+    }
