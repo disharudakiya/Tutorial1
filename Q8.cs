aa2fb604ec397d98ec5f81815ace1e87fb6c4f6a
disharudakiya <141400666+disharudakiya@users.noreply.github.com>
2025-07-07 19:37:37 +0530

    Q8

---
+++ Q8.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial1
+{
+    internal class Q8
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name : ");
+            string intput = Console.ReadLine();
+            string uppername = intput.ToUpper();
+            Console.WriteLine( uppername);
+        }
+
+
+
+    }
+}
