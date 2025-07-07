596c7dd284e62b4792163603416d25f078807b16
disharudakiya <141400666+disharudakiya@users.noreply.github.com>
2025-07-07 19:51:56 +0530

    modif --commit

---
+++ Q9.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial1
+{
+    internal class Q9
+    {
+        static void Main(string[] args)
+        {
+            string input = "JoHn F kEnNedy";
+            string toggle = input.
+            }
+        }
+    }
+
